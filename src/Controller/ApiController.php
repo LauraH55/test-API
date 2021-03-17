327794f32839fe6614568a248a964e773b0496e2
Laura Hantz <laura.hantz@hotmail.fr>
2021-03-17 16:29:48 +0100

    Test Api + routes

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController extends AbstractController
+{
+    /**
+     * @Route("/", name="api_home", methods="GET")
+     */
+    public function home(): Response
+    {
+        $users = [
+            'Emilie'=> 'emilie@emilie.com',
+            'Kevin' => 'kevin@kevin.com',
+            'Alexia' => 'alexia@alexia.com',
+            'Yael' => 'yael@yael.com',
+            'Laura' => 'laura@laura.com'
+        ];
+
+        return $this->json($users);
+    }
+
+    /**
+     * @Route("/api-roles", name="api_roles", methods="GET")
+     */
+    public function roles()
+    {
+        $roles = [
+            0 => 'Scrum Master',
+            1 => 'Product Owner',
+            2 => 'Lead Dev Front',
+            3 => 'Lead Dev Back',
+            4 => 'Git Master',
+        ];
+
+        return $this->json($roles);
+    }
+}
